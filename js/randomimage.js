751cbb7b687ff7d02f277af573255bf9d4fb1ad2
ees-ja perekonnanimi <svetik24@hotmail.com>
2024-09-17 16:07:52 +0300

    add week11 jssandbox

---
+++ js/randomimage.js
@@ -0,0 +1,11 @@
+const myButton = document.querySelector('button');
+const image = document.querySelector('img');
+const images = ["img1.jpg","img2.jpg","img3.jpg","img4.jpg"];
+
+myButton.addEventListener('click', changeImage);
+
+function changeImage() {
+
+    let randomIndex = Math.floor(Math.random() * images.length);
+    image.src = "images/" + images[randomIndex];
+}
